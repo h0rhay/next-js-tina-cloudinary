b9591924b73bb7449e98c2603051409166d71e92
George clark <george_clark@georges-mbp-2.home>
2022-07-21 17:39:10 +0100

    update var

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -369,7 +369,7 @@ const branch = "main";
 const apiURL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:4001/graphql"
-    : `https://content.tinajs.io/content/${process.env.CLIENT_ID}/github/${branch}`;
+    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
 
 export const tinaConfig = defineConfig({
   apiURL,
